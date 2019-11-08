ad63107b5618228d8e4ebb4daa6da432e72eee47
JackMarx <seriousfools@gmail.com>
2019-11-08 09:25:56 -0600

    netlify preped

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import axios from "axios";
 
 axios.defaults.baseURL = 
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://murmuring-river-61554.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "https://joshcookbook.herokuapp.com" : "https://joshcookbook.herokuapp.com/api/recipes";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
